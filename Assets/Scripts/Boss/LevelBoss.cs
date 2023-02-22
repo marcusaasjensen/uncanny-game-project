99b779203e12b2aa4c466aa2b3bbad36511026c2
Marcus Aas Jensen <aasjensenm@gmail.com>
2023-02-22 14:25:32 +0100

    Fix of stopping action sequence when game over.

--- Assets/Scripts/Boss/LevelBoss.cs
+++ Assets/Scripts/Boss/LevelBoss.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public abstract class LevelBoss : MonoBehaviour
 {
     [SerializeField] List<GameObject> _levelElements;
+    [SerializeField] bool _isLevelCompleted = false;
+
+    IEnumerator _actionSequenceCoroutine;
 
     [ContextMenu("Start Sequence")]
-    public void StartActionSequence() => StartCoroutine(ActionSequence());
-    public void StopActionSequence() => StopCoroutine(ActionSequence());
+
+    void Start()
+    {
+        this.gameObject.SetActive(true);    
+    }
+    public void StartActionSequence()
+    {
+         StartCoroutine(ActionSequence());
+        _actionSequenceCoroutine = ActionSequence();
+    }
+    public void StopActionSequence() 
+    { 
+        StopCoroutine(_actionSequenceCoroutine);
+        this.gameObject.SetActive(false);
+    }
     public abstract IEnumerator ActionSequence();
 }
